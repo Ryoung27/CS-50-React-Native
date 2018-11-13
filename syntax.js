a9839b976286414123e977d7961a942f40ca2068
Ryoung27 <RRYoung89@gmail.com>
2018-11-13 13:44:19 -0600

    finished lecture one

--- syntax.js
+++ syntax.js
@@ -8,4 +8,67 @@ const arry = ["string",
 function(){ console.log('hi')},
 ]
 
-arry[2]
+arry[2]()
+
+for (let i = 0; i < arry.length; i++){
+    console.log(arr[i])
+}
+
+const x = 42
+
+console.log(typeof 42)
+
+const o = new Object()
+o.firstName = "Ronnie"
+o.lastName = "Young"
+o.isLearning = true
+o.greet = function(){
+    console.log('hi')
+}
+
+const o2 = {}
+o.firstName = "Ronnie"
+o['lastName'] = "Young"
+const key = "isLearning"
+o[key] = true
+o['greet'] = function(){
+    console.log('hi')
+}
+
+const o3 = {
+    firstName: "Ronnie",
+    lastName: "Young",
+    isLearning: true,
+    greet: function(){
+        console.log('hi')
+    },
+    address: {
+        street: "Main St.",
+        number: 123
+    }
+}
+
+o3.address
+
+03.address.number
+
+const x = {
+    a: 'a',
+    b: 'b'
+}
+
+const x2 = x
+
+x.a = "new value"
+
+console.low(x2.a)
+
+//Prototype inhertiance
+
+const arr = []
+
+arr.push('value')
+arr.push('value two')
+
+
+
