f3bc53b4ac5657066d8f0fd4eb54d691ec6132f5
Ryoung27 <RRYoung89@gmail.com>
2018-11-14 14:32:42 -0600

    async

--- lecture1.js
+++ lecture1.js
@@ -17,3 +17,37 @@ function add(x, y){
 
 add(1, 2);
 x.reduce(add)
+
+//Higher order Functions
+function map(arr, fn){
+    const newArr = []
+
+    for (let i = 0; i < arr.length; i++){
+        let val = arr[i]
+        newArr.push(fn(val))
+    }
+
+    return newArr;
+}
+
+function addOne(num){
+    return num + 1
+}
+
+const x = [0,1,2,3,4]
+
+console.log(map(x, addOne))
+
+//An attempt to fill up the stack
+// function recurse(){
+//     console.log('recursion');
+//     return recurse()
+// }
+
+
+//Async function
+function doSomething(callback){
+    setTimeOut(function() {callback(1)}, 1000)
+}
+
+doSomething(console.log)
